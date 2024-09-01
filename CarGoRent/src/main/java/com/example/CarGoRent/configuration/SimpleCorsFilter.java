5cd35ea45ca2b04a420b9bba10c8a87c894ca15a
Prince <banerjee.prince69@gmail.com>
2024-09-01 19:03:16 +0530

    WIP

---
+++ CarGoRent/src/main/java/com/example/CarGoRent/configuration/SimpleCorsFilter.java
@@ -0,0 +1,22 @@
+package com.example.CarGoRent.configuration;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class SimpleCorsFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException{
+        HttpServletResponse response = (HttpServletResponse) res;
+        HttpServletRequest request = (HttpServletRequest) req;
+
+    }
+}
