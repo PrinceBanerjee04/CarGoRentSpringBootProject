601ea40210b032589f076603a112776821918eb4
Prince <banerjee.prince69@gmail.com>
2024-09-01 11:51:35 +0530

    Initial Commit

---
+++ CarGoRent/src/test/java/com/example/CarGoRent/CarGoRentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CarGoRent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarGoRentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
